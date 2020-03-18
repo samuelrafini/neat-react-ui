590e3fb0490a840599e6d195d6ea0274f8131ccc
samuel rafini <samuelrafini@gmail.com>
2020-03-18 13:00:11 +0100

    finished button component

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'neat-react-ui'
+import {Button} from 'neat-react-ui'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Button variant="raised" className="button" neatColor="primary" loading={true}>Text comes here to test</Button>
+        <Button shape="full-rounded" variant='flat' iconRight={<img />} iconLeft={<img />} neatColor='primary'>button 1</Button>
+        <Button variant='flat' loading={true} color='red'>button 1</Button>
+        <Button shape="rounded" iconLeft={<img />} neatColor='success'>button 1</Button>
       </div>
     )
   }
